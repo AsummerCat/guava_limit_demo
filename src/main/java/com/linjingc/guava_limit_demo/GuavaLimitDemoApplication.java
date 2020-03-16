4e972c997443bd052a1eec4b67706a44441970d6
chenxiaocehng <583188551@qq.com>
2020-03-16 17:33:00 +0800

    Initial commit

---
+++ src/main/java/com/linjingc/guava_limit_demo/GuavaLimitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.guava_limit_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaLimitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuavaLimitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linjingc/guava_limit_demo/GuavaLimitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.guava_limit_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuavaLimitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
